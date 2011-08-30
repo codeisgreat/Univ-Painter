acf8d963d474d32530b0ff214a86481e7a32c297
jrillet <jrillet@jrillet.pardus.aspire>
2011-08-30 18:59:26 +0900

    The Initialization.

---
+++ Painter/Painter.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Painter
+{
+    partial class Painter
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.numLineWidth = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btLoad = new System.Windows.Forms.Button();
+            this.btSave = new System.Windows.Forms.Button();
+            this.btColor = new System.Windows.Forms.Button();
+            this.cbLine = new System.Windows.Forms.CheckBox();
+            this.cbPen = new System.Windows.Forms.CheckBox();
+            this.paintzone = new System.Windows.Forms.PictureBox();
+            this.colorBox = new System.Windows.Forms.ColorDialog();
+            this.saveBox = new System.Windows.Forms.SaveFileDialog();
+            this.openBox = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numLineWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paintzone)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.numLineWidth);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.btLoad);
+            this.splitContainer1.Panel1.Controls.Add(this.btSave);
+            this.splitContainer1.Panel1.Controls.Add(this.btColor);
+            this.splitContainer1.Panel1.Controls.Add(this.cbLine);
+            this.splitContainer1.Panel1.Controls.Add(this.cbPen);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.paintzone);
+            this.splitContainer1.Size = new System.Drawing.Size(884, 471);
+            this.splitContainer1.SplitterDistance = 147;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // numLineWidth
+            // 
+            this.numLineWidth.Location = new System.Drawing.Point(68, 168);
+            this.numLineWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numLineWidth.Name = "numLineWidth";
+            this.numLineWidth.Size = new System.Drawing.Size(37, 19);
+            this.numLineWidth.TabIndex = 8;
+            this.numLineWidth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "線の太さ:";
+            // 
+            // btLoad
+            // 
+            this.btLoad.Location = new System.Drawing.Point(12, 383);
+            this.btLoad.Name = "btLoad";
+            this.btLoad.Size = new System.Drawing.Size(75, 23);
+            this.btLoad.TabIndex = 6;
+            this.btLoad.Text = "開く";
+            this.btLoad.UseVisualStyleBackColor = true;
+            this.btLoad.Click += new System.EventHandler(this.btLoad_Click);
+            // 
+            // btSave
+            // 
+            this.btSave.Location = new System.Drawing.Point(12, 354);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(75, 23);
+            this.btSave.TabIndex = 5;
+            this.btSave.Text = "保存";
+            this.btSave.UseVisualStyleBackColor = true;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // btColor
+            // 
+            this.btColor.BackColor = System.Drawing.Color.Black;
+            this.btColor.Location = new System.Drawing.Point(12, 113);
+            this.btColor.Name = "btColor";
+            this.btColor.Size = new System.Drawing.Size(50, 50);
+            this.btColor.TabIndex = 4;
+            this.btColor.UseVisualStyleBackColor = false;
+            this.btColor.Click += new System.EventHandler(this.btColor_Click);
+            // 
+            // cbLine
+            // 
+            this.cbLine.Appearance = System.Windows.Forms.Appearance.Button;
+            this.cbLine.AutoSize = true;
+            this.cbLine.Location = new System.Drawing.Point(12, 40);
+            this.cbLine.Name = "cbLine";
+            this.cbLine.Size = new System.Drawing.Size(39, 22);
+            this.cbLine.TabIndex = 3;
+            this.cbLine.Text = "直線";
+            this.cbLine.UseVisualStyleBackColor = true;
+            this.cbLine.Click += new System.EventHandler(this.cbLine_Click);
+            // 
+            // cbPen
+            // 
+            this.cbPen.Appearance = System.Windows.Forms.Appearance.Button;
+            this.cbPen.AutoSize = true;
+            this.cbPen.Checked = true;
+            this.cbPen.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbPen.Location = new System.Drawing.Point(12, 12);
+            this.cbPen.Name = "cbPen";
+            this.cbPen.Size = new System.Drawing.Size(34, 22);
+            this.cbPen.TabIndex = 2;
+            this.cbPen.Text = "ペン";
+            this.cbPen.UseVisualStyleBackColor = true;
+            this.cbPen.Click += new System.EventHandler(this.cbPen_Click);
+            // 
+            // paintzone
+            // 
+            this.paintzone.BackColor = System.Drawing.Color.White;
+            this.paintzone.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.paintzone.Location = new System.Drawing.Point(0, 0);
+            this.paintzone.Name = "paintzone";
+            this.paintzone.Size = new System.Drawing.Size(733, 471);
+            this.paintzone.TabIndex = 0;
+            this.paintzone.TabStop = false;
+            this.paintzone.Paint += new System.Windows.Forms.PaintEventHandler(this.paintzone_Paint);
+            this.paintzone.MouseDown += new System.Windows.Forms.MouseEventHandler(this.paintzone_MouseDown);
+            this.paintzone.MouseMove += new System.Windows.Forms.MouseEventHandler(this.paintzone_MouseMove);
+            this.paintzone.MouseUp += new System.Windows.Forms.MouseEventHandler(this.paintzone_MouseUp);
+            // 
+            // saveBox
+            // 
+            this.saveBox.Filter = "JPEG 画像 (*.jpg) | *.jpg; *jpeg | ビットマップ | *.bmp";
+            // 
+            // openBox
+            // 
+            this.openBox.Filter = "JPEG 画像 (*.jpg) | *.jpg; *jpeg | ビットマップ | *.bmp";
+            // 
+            // Painter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 471);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Painter";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numLineWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paintzone)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btColor;
+        private System.Windows.Forms.CheckBox cbLine;
+        private System.Windows.Forms.CheckBox cbPen;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Button btLoad;
+        private System.Windows.Forms.ColorDialog colorBox;
+        private System.Windows.Forms.SaveFileDialog saveBox;
+        private System.Windows.Forms.OpenFileDialog openBox;
+        private System.Windows.Forms.PictureBox paintzone;
+        private System.Windows.Forms.NumericUpDown numLineWidth;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Painter/Painter.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Painter
+{
+    public partial class Painter : Form
+    {
+        /// <summary>現在の描画ツール。デフォルトはペン (手書き)</summary>
+        private Instrument currentInstrument = Instrument.Pen;
+        /// <summary>グラフィクスオブジェクト</summary>
+        private Graphics g;
+
+        /// <summary>ペンで描画する際に必要なグラフィックパス</summary>
+        private GraphicsPath path = new GraphicsPath();
+
+        /// <summary>
+        /// 描画している最中であるか否か。
+        /// 即ち、一回クリックして始点を指定してから、もう一回クリックして終点を指定する間の状態であるか否か。
+        /// </summary>
+        private bool isDrawing = false;
+        /// <summary>直線を描画する際の始点の座標</summary>
+        private Point start = new Point(-1, -1);
+
+        /// <summary>
+        /// フォームを構成します。
+        /// </summary>
+        public Painter()
+        {
+            InitializeComponent();
+
+            paintzone.Image = new Bitmap(paintzone.Width, paintzone.Height);
+            g =  Graphics.FromImage(paintzone.Image);
+            g.FillRectangle(new SolidBrush(paintzone.BackColor), 0, 0, paintzone.Width, paintzone.Height); // 保存時に背景色が、paintzone.BackColor になるように、背景色を設定。これがないと、背景が透明色で保存される。
+
+        }
+
+        /// <summary>
+        /// マウスが押された時の動作です。
+        /// </summary>
+        private void paintzone_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return; // 左ボタンが押された状態でなければ、なにもしない。
+
+            /* ----- ペンの時のクリック ----- */
+            if (currentInstrument == Instrument.Pen)
+            {
+                isDrawing = true;
+                path = new GraphicsPath(); // マウスが押される度に、新たにグラフィックパスを作成
+                path.StartFigure(); // グラフィックパスの描画を開始
+                paintzone.Invalidate(); // paintzone を再描画
+            }
+            /* ----- 直線の時のクリック ----- */
+            else // if (currentInstrument == Instrument.Line)
+            {
+                if (!isDrawing) // 現在描画中でなければ
+                {
+                    start = paintzone.PointToClient(MousePosition);
+                    isDrawing = true;
+                }
+                else // if (isDrawing) // 現在描画中であれば
+                {
+                    Point end = paintzone.PointToClient(MousePosition); // 終点を取得
+
+                    g.DrawLine(new Pen(colorBox.Color, (float)numLineWidth.Value), start, end); // 描画
+
+                    isDrawing = false; // 描画中ではない、という通知をする。
+
+                    paintzone.Invalidate(); // paintzone を再描画
+                }
+            }
+        }
+
+        private void paintzone_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (currentInstrument == Instrument.Pen && isDrawing) // ペンで描画中の時は、
+            {
+                Point clicked = paintzone.PointToClient(MousePosition); // 現在のマウスの場所を取得
+                path.AddLine(clicked, clicked); // パスに書きこむ
+            }
+            paintzone.Invalidate(); // マウスが移動中には、ペイントゾーンを再描画
+        }
+
+        /// <summary>マウスの押されなくなったときの処理です。主にペンの描画の際に、描画を止めるために用います。</summary>
+        private void paintzone_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (currentInstrument == Instrument.Pen)
+                isDrawing = false;
+        }
+
+        private void paintzone_Paint(object sender, PaintEventArgs e)
+        {
+            if (currentInstrument == Instrument.Line && isDrawing) // 直線記述中の時
+            {
+                e.Graphics.DrawLine(new Pen(colorBox.Color, (float)numLineWidth.Value), start, paintzone.PointToClient(MousePosition));
+            }
+            else if (currentInstrument == Instrument.Pen && isDrawing) // ペンで描画中の時
+            {
+                g.DrawPath(new Pen(colorBox.Color, (float)numLineWidth.Value), path); // 描画
+            }
+        }
+
+
+        // ---------- ボタンの処理 ----------
+
+        /// <summary>ペンボタンがクリックされた時</summary>
+        private void cbPen_Click(object sender, EventArgs e)
+        {
+            currentInstrument = Instrument.Pen;
+            cbPen.Checked = true;
+            cbLine.Checked = false;
+        }
+
+        /// <summary>直線ボタンがクリックされた時</summary>
+        private void cbLine_Click(object sender, EventArgs e)
+        {
+            currentInstrument = Instrument.Line;
+            cbLine.Checked = true;
+            cbPen.Checked = false;
+        }
+
+        /// <summary>色ボタンが押された時</summary>
+        private void btColor_Click(object sender, EventArgs e)
+        {
+            colorBox.ShowDialog();
+            btColor.BackColor = colorBox.Color;
+        }
+
+        /// <summary>保存ボタンが押された時</summary>
+        private void btSave_Click(object sender, EventArgs e)
+        {
+            DialogResult result = saveBox.ShowDialog();
+            if (result == DialogResult.OK)
+                paintzone.Image.Save(saveBox.FileName);
+        }
+
+        /// <summary>開くボタンが押された時</summary>
+        private void btLoad_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openBox.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                // paintzone.Image = Image.FromFile(openBox.FileName); // paintzone に読み込み
+                g.DrawImage(Image.FromFile(openBox.FileName), 0, 0, paintzone.Image.Width, paintzone.Image.Height);
+                paintzone.Invalidate();
+            }
+        }
+
+        /// <summary>描画ツールの種類を表す列挙体です。</summary>
+        enum Instrument
+        {
+            Pen, Line
+        }
+    }
+}
